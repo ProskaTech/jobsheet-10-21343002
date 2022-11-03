83f9c573fc1c116edeb50724d7341ffe334565fc
21343002 <112646969+21343002@users.noreply.github.com>
2022-11-03 18:51:51 +0700

    Add files via upload

---
+++ JS-10/src/Smartphone/AndroMain.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Smartphone;
+
+/**
+ *
+ * Created By 21343002_Aini Rizkina
+ */
+public class AndroMain {
+    public static void main(String[] args) {
+        Android andro = new Android();
+        
+        andro.Nyala();
+        andro.Panggilan();
+        andro.SMS();
+        andro.Shutdown();
+    }
+}
+
+
---
+++ JS-10/src/Smartphone/Android.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Smartphone;
+
+/**
+ *
+ * Created By 21343002_Aini Rizkina
+ */
+public class Android {
+   public void Nyala(){
+        System.out.println("Android Menyala");
+    }
+    public void Panggilan(){
+        System.out.println("Kring Kring Kring, ada panggilan masuk");
+    }
+    public void SMS(){
+        System.out.println("Tenenoeeeet, ada SMS baru");
+    }
+    public void Shutdown(){
+        System.out.println("Android Android dimatikan");
+    }
+}
